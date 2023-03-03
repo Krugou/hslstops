77317d492fb461478108c198b53bbc640421b9f5
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-03 03:53:03 +0200

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+
+// obtain the user's current location using HTML5 geolocation
+navigator.geolocation.getCurrentPosition(function (position) {
+    // var lat = position.coords.latitude;
+    // var lng = position.coords.longitude;
+    var lat = 60.2082749074403;
+    var lng = 24.66534874564775;
+    // send a request to the Overpass API to obtain the nearest stops in the HSL network
+    var overpassUrl = 'https://overpass-api.de/api/interpreter?data=[out:json];node["public_transport"="stop_position"](around:700,' + lat + ',' + lng + ');out;';
+    fetch(overpassUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // parse the response from the Overpass API to extract the relevant information about the nearest stops
+            var stops = [];
+            data.elements.forEach(function (element) {
+                var stop = {
+                    name: element.tags.name,
+                    lat: element.lat,
+                    lng: element.lon
+                };
+                stops.push(stop);
+            });
+
+            // display the map using Leaflet
+            var map = L.map('map').setView([lat, lng], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            // add markers to the map for each of the nearest stops
+            stops.forEach(function (stop) {
+                var marker = L.marker([stop.lat, stop.lng]).addTo(map);
+                marker.bindPopup(stop.name);
+
+                // add the stop name to the list in the body of the page
+                var listItem = document.createElement('li');
+                listItem.innerText = stop.name;
+                document.getElementById('stops').appendChild(listItem);
+            });
+        });
+});
